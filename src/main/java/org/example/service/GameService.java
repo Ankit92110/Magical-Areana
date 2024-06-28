a2f9572913135aeee2417ea7dc46d59601d418aa
Ankit92110 <ankitmishra28799@gmail.com>
2024-06-29 00:32:02 +0530

    service layer has been created for the purpose of play match

---
+++ src/main/java/org/example/service/GameService.java
@@ -0,0 +1,24 @@
+package org.example.service;
+
+import org.example.entity.Player;
+import org.example.repository.PlayerRepo;
+import org.example.util.Dice;
+
+import java.util.List;
+
+public class GameService implements Game{
+    private final PlayerRepo playerRepo;
+    private final Dice attackingDice;
+    private final Dice defendingDice;
+
+    public GameService(PlayerRepo playerRepo, Dice attackingDice, Dice defendingDice) {
+        this.playerRepo = playerRepo;
+        this.attackingDice = attackingDice;
+        this.defendingDice = defendingDice;
+    }
+
+    @Override
+    public void play() {
+
+    }
+}
